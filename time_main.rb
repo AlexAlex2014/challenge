21076c069a9aa2543e7c978137a6c20dbd19f327
Alex <azuev061981@gmail.com>
2019-07-04 17:00:56 +0300

    first commit

---
+++ time_main.rb
@@ -0,0 +1,8 @@
+class TimeMain
+  attr_accessor :time, :minutes
+  def initialize(time, minutes)
+    @time = time
+    @minutes = minutes
+  end
+
+end
---
+++ time_service.rb
@@ -0,0 +1,11 @@
+require_relative 'time_main'
+require_relative 'time'
+
+module TimeService
+
+
+
+
+
+
+end
